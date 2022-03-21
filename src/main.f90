de4c63fca6fb2b6d95646445626fdd4fd1946c61
毕洲华 <bzhouha@Foliage.local>
2022-03-21 20:28:19 +0800

    rebulid the IO module : now support raw, binary and hdf5 files

--- src/main.f90
+++ src/main.f90
@@ -7,16 +7,16 @@ program main
     use petsc
     implicit none
     PetscErrorCode :: ierr
-    
+
     call PetscInitialize(PETSC_NULL_CHARACTER,ierr)
     if (ierr /= 0) then
         write(*,*) 'PetscInitialize failed'
         stop
     endif
-    
+
     call istream(PETSC_COMM_WORLD)
-    ! call working(PETSC_COMM_WORLD)              
-    ! call ostream(PETSC_COMM_WORLD)
+    call working(PETSC_COMM_WORLD)              
+    call ostream(PETSC_COMM_WORLD)
 
     call PetscFinalize(ierr)
 end program main
