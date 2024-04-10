d7c5023df555c27bd795f097671652adbfc7d3c4
毕洲华 <bzhouha@Foliage.local>
2024-04-10 10:06:09 +0800

    add License

--- src/mod_iterate.f90
+++ src/mod_iterate.f90
@@ -1,14 +1,19 @@
 !------------------------------------------------------------------------------
 !
-! Copyright (C) 2019-2024 Bzhouha
+! Copyright (c) 2019-2024 Bzhouha 
+! All rights reserved.
 ! 
 ! This file is part of START_LNS.
-!
 ! START_LNS is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-!
 ! START_LNS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-!
 ! You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>. 
+!
+!------------------------------------------------------------------------------
+!
+! This work used the PETSc library, which is developed by the PETSc Development Team. See https://petsc.org/ for more information.
+! Copyright (c) 2023, PETSc Development Team
+! All rights reserved.
+! This file is subject to the terms and conditions of the BSD 2-Clause License. See the file LICENSE in the top-level directory for more information.
 !
 !------------------------------------------------------------------------------
 
